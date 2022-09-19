17007c175882ba7079f7aa97c794602ea62045d8
Andreas Guther <andreas.guther@airbus.com>
2022-09-19 16:52:44 +0200

    feat: added CentOS 7 variant

---
+++ rootfs.centos.7/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# clean up
+rm -rf /tmp/.X0-lock
+rm -rf /tmp/.X11-unix
+rm -rf /tmp/.ICE-unix
+
+# start supervisor
+exec /usr/bin/supervisord \
+    --nodaemon \
+    --configuration /etc/supervisord.conf
---
+++ run.centos.7.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker run \
+    --detach \
+    --rm \
+    --env http_proxy=$http_proxy \
+    --env https_proxy=$https_proxy \
+    --env no_proxy="$no_proxy" \
+    --publish 6080:6080 \
+    --name docker-xfce \
+    docker-xfce:centos-7-latest
